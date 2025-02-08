7be5028d44aace0c856cddad68e6ae44f2a8ad1e
Nazarenee <cph-cm425@cphbusiness.dk>
2025-02-08 19:08:10 +0100

    mere stuff

--- src/main/java/com/example/demo/Student/StudentService.java
+++ src/main/java/com/example/demo/Student/StudentService.java
@@ -1,27 +1,63 @@
 package com.example.demo.Student;
 
+import jakarta.persistence.Transient;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 
 @Service
 public class StudentService {
 
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+
     public List<Student> getStudents(){
-        return List.of(
-                new Student(1L,
-                        "mariam",
-                        "mariam.jamal@gmail.com",
-                        LocalDate.of(2000, Month.JANUARY,5),
-                        21),new Student(1L,
-                        "jonas",
-                        "Jonas@gmail.com",
-                        LocalDate.of(2000, Month.JANUARY,5),
-                        21)
-        );
+        return studentRepository.findAll();
+    }
+
+    public void addNewStudent(Student student){
+        Optional<Student> studentOptional = studentRepository.findStudentByEmail(student.getEmail());
+
+        if(studentOptional.isPresent()){
+            throw new IllegalStateException("email taken");
+        }
+
+        studentRepository.save(student);
+    }
+
+    public void deleteStudent(Long id){
+        boolean studentExist= studentRepository.existsById(id);
+        if(!studentExist){
+            throw new IllegalStateException("student with id "+id+" does not exist");
+        }
+        studentRepository.deleteById(id);
+    }
+
+     @Transactional
+    public void updateStudent(Long id, String name, String email){
+        Student student = studentRepository.findById(id).orElseThrow(()-> new IllegalStateException("student with id" + id + "does not exist"));
+        if(name!= null && name.length() > 0 && !Objects.equals(student.getName(), name)){
+            student.setName(name);
+
+        }
+
+         if(email  != null && !Objects.equals(student.getEmail(), email)){
+                 student.setEmail(email);
+
+         }
+
     }
 }
